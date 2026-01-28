3ce2324ffe306e4a6846d2de7fec60633a4c66b1
Hossein Soleimani1 <Hossein.Soleimani@SABALIFT.COM>
2026-01-28 08:35:14 +0330

    contact app

---
+++ vite-project/src/components/Contacts.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import{v4} from "uuid"
+
+import ContactsList from "./ContactsList";
+import inputs from "../constants/inputs";
+
+function Contacts(){
+    const[contacts,setContacts]=useState([]);
+    const[alert,setAlert]=useState("");
+    const[contact,setContact]=useState({
+        id:"",
+        name:"",
+        lastName:"",
+        email:"",
+        phone:""
+    });
+
+    const changeHandler=(event)=>{
+        const name=event.target.name;
+        const value=event.target.value;
+
+        setContact((contact)=>({...contact,[name]:value}))
+    }
+
+    const addHandler=()=>{
+        if(!contact.name || !contact.lastName || !contact.email || !contact.phone){
+            setAlert("Please Enter valid data!");
+            return;
+        }
+        
+        setAlert("")
+        const newContact={...contact, id: v4()}
+        setContacts(contacts=>([...contacts,newContact]))
+        setContact({
+        name:"",lastName:"",email:"",phone:""
+    })
+    }
+
+    return(
+        <div>
+            <div>
+                {
+                inputs.map((input, index)=>(<input
+                    key={index}
+                    type={input.type}
+                    name={input.name}
+                    placeholder={input.placeholder}
+                    value={contact[input.name]}
+                    onChange={changeHandler} />))
+                }
+                <button onClick={addHandler}>Add Contact</button>
+            </div>
+            <div>{alert && <p>{alert}</p>}</div>
+            <ContactsList contacts={contacts}/>
+        </div>
+    )
+}
+
+export default Contacts
---
+++ vite-project/src/components/ContactsList.jsx
@@ -0,0 +1,14 @@
+
+function ContactsList({contacts}){
+    
+    return(
+        <div>
+            <h3>Contacts List</h3>
+            <ul>
+                {contacts.map(contact=>(<li key={contact.id}>{contact.name}</li>))}
+            </ul>
+        </div>
+    )
+}
+
+export default ContactsList
